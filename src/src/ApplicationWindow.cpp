568d040a40263143d8f42b182e01a40d633ef974
catNull <benvb2004@gmail.com>
2022-12-24 01:41:22 +0100

    idk what i added... is there a difference, who knows *shrugs*

--- src/src/ApplicationWindow.cpp
+++ src/src/ApplicationWindow.cpp
@@ -1,12 +1,12 @@
 #include "ApplicationWindow.h"
 
 #include <array>
+#include <cairo/cairo-xlib.h>
 #include <cstdio>
 #include <iostream>
 #include <memory>
 #include <stdexcept>
 #include <string>
-#include <cairo/cairo-xlib.h>
 
 unsigned long _RGB(int r, int g, int b) { return b + (g << 8) + (r << 16); }
 
@@ -22,7 +22,16 @@ ApplicationWindow::ApplicationWindow() {
     int w = DisplayWidth(display, s);
     int h = DisplayHeight(display, s);
 
-    window = XCreateSimpleWindow(display, RootWindow(display, s), 0, 0, 100, 100, 1, BlackPixel(display, s), WhitePixel(display, s));
+    XVisualInfo vinfo;
+    XMatchVisualInfo(display, DefaultScreen(display), 32, TrueColor, &vinfo);
+
+    XSetWindowAttributes attr;
+    attr.colormap         = XCreateColormap(display, DefaultRootWindow(display), vinfo.visual, AllocNone);
+    attr.border_pixel     = 0;
+    attr.background_pixel = 0;
+
+    window = XCreateWindow(display, DefaultRootWindow(display), 0, 0, 300, 200, 0, vinfo.depth, InputOutput, vinfo.visual, CWColormap | CWBorderPixel | CWBackPixel, &attr);
+    //window = XCreateSimpleWindow(display, RootWindow(display, s), 0, 0, 100, 100, 1, BlackPixel(display, s), WhitePixel(display, s));
 
     XSelectInput(display, window, ExposureMask | KeyPressMask);
     XMapWindow(display, window);
@@ -36,6 +45,7 @@ ApplicationWindow::ApplicationWindow() {
     ic = XCreateIC(im, XNInputStyle, XIMPreeditNothing | XIMStatusNothing, XNClientWindow, window, NULL);
 
     XSetICFocus(ic);
+
 
     DeleteWindowMessage = XInternAtom(display, "WM_DELETE_WINDOW", False);
     XSetWMProtocols(display, window, &DeleteWindowMessage, 1);
@@ -99,30 +109,30 @@ void ApplicationWindow::Start() {
             }
         }
 
-        cairo_select_font_face (CR, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-        cairo_set_font_size (CR, 90.0);
+        cairo_select_font_face(CR, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+        cairo_set_font_size(CR, 90.0);
 
-        cairo_move_to (CR, 10.0, 500.0);
-        cairo_show_text (CR, t.str.c_str());
+        cairo_move_to(CR, 10.0, 500.0);
+        cairo_show_text(CR, t.str.c_str());
 
         cairo_pattern_t *pat;
 
-        pat = cairo_pattern_create_linear (0.0, 0.0,  0.0, 256.0);
-        cairo_pattern_add_color_stop_rgba (pat, 1, 0, 0, 0, 1);
-        cairo_pattern_add_color_stop_rgba (pat, 0, 1, 1, 1, 1);
-        cairo_rectangle (CR, 0, 0, 256, 256);
-        cairo_set_source (CR, pat);
-        cairo_fill (CR);
-        cairo_pattern_destroy (pat);
+        pat = cairo_pattern_create_linear(0.0, 0.0, 0.0, 256.0);
+        cairo_pattern_add_color_stop_rgba(pat, 1, 0, 0, 0, 1);
+        cairo_pattern_add_color_stop_rgba(pat, 0, 1, 1, 1, 1);
+        cairo_rectangle(CR, 0, 0, 256, 256);
+        cairo_set_source(CR, pat);
+        cairo_fill(CR);
+        cairo_pattern_destroy(pat);
 
-        pat = cairo_pattern_create_radial (115.2, 102.4, 25.6,
-                102.4,  102.4, 128.0);
-        cairo_pattern_add_color_stop_rgba (pat, 0, 1, 1, 1, 1);
-        cairo_pattern_add_color_stop_rgba (pat, 1, 0, 0, 0, 1);
-        cairo_set_source (CR, pat);
-        cairo_arc (CR, 128.0, 128.0, 76.8, 0, 2 * 3.1415);
-        cairo_fill (CR);
-        cairo_pattern_destroy (pat);
+        pat = cairo_pattern_create_radial(115.2, 102.4, 25.6,
+                                          102.4, 102.4, 128.0);
+        cairo_pattern_add_color_stop_rgba(pat, 0, 1, 1, 1, 1);
+        cairo_pattern_add_color_stop_rgba(pat, 1, 0, 0, 0, 1);
+        cairo_set_source(CR, pat);
+        cairo_arc(CR, 128.0, 128.0, 76.8, 0, 2 * 3.1415);
+        cairo_fill(CR);
+        cairo_pattern_destroy(pat);
 
         //std::cout << charBuffer->size() << std::endl;
         XDrawString(display, window, DefaultGC(display, s), 50, 50, t.str.c_str(), t.str.size());
