a267d706d2a9283ad7ac3aa0d8b3ce4c5b39be06
Lasse Lykke Jensen <lasselj@yandex.com>
2022-12-24 03:39:16 +0100

    Switch to pango for font rendering again

--- src/src/Terminal.cpp
+++ src/src/Terminal.cpp
@@ -1,6 +1,7 @@
 #include "Terminal.h"
 
 #include <iostream>
+#include <pango/pangocairo.h>
 
 
 void Terminal::PressChar(std::string inpString, int keysym, int status) {
@@ -12,12 +13,12 @@ void Terminal::PressChar(std::string inpString, int keysym, int status) {
 }
 
 void Terminal::Draw(cairo_t *CR) {
-    cairo_select_font_face(CR, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
-    cairo_set_font_size(CR, 12.0);
-
-    cairo_font_options_t *font_options = cairo_font_options_create();
-
     cairo_set_source_rgb(CR, 0.8, 0.8, 0.8);
+
+    PangoLayout *layout = pango_cairo_create_layout(CR);
+    PangoFontDescription *desc = pango_font_description_from_string("Hack Normal 9");
+    pango_layout_set_font_description(layout, desc);
+    pango_font_description_free(desc);
 
     double x = 5;
     double y = 12 + 5;
@@ -26,8 +27,16 @@ void Terminal::Draw(cairo_t *CR) {
         cairo_move_to(CR, x, y);
 
         for (char c : line) {
-            cairo_show_text(CR, &c);
+            pango_layout_set_text(layout, &c, 1);
+            pango_cairo_update_layout(CR, layout);
+            pango_cairo_show_layout(CR, layout);
+
+            int charW, charH;
+            pango_layout_get_size (layout, &charW, &charH);
+
             cairo_get_current_point(CR, &x, &y);
+            x += ((double)charW/PANGO_SCALE);
+            cairo_move_to(CR, x, y);
             if (x >= width-12) {
                 x = 5;
                 y += 20;
@@ -38,6 +47,8 @@ void Terminal::Draw(cairo_t *CR) {
         x = 5;
         y += 20;
     }
+
+    g_object_unref(layout);
 }
 
 bool Terminal::SetTermProperties(int _x, int _y, int _width, int _height) {
